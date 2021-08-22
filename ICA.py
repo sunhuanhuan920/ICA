5a4871633c4b4f3ebce6f23e6cc94239d525ea79
Zhenhuann Sun <sunsteven11@gmail.com>
2021-08-22 16:58:09 -0400

    Created wav file using data matrix inside Signals folder

--- ICA.py
+++ ICA.py
@@ -1,6 +1,19 @@
 import numpy as np
 import scipy.io.wavfile
 
+class ICA():
+    def __init__(self):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def transform(self, X):
+        pass
+
 # load data
 X = np.loadtxt("./Data/mix.dat")
-print(X.shape)
+
+# convert numerical matrix data back to sound wave file
+for i in range(X.shape[1]):
+    scipy.io.wavfile.write("Signals/input/mixed_{}.wav".format(i), 11025, X[:, i])
