29f2a3cb35d66fa4feef6d1308c187e1773e1d90
Zhenhuann Sun <sunsteven11@gmail.com>
2021-08-23 14:56:36 -0400

    Updated Signals folder

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ X = np.loadtxt("./Data/mix.dat")
 
 # convert numerical matrix data back to sound wave file
 for i in range(X.shape[1]):
-    scipy.io.wavfile.write("./Signals/input/mixed_{}.wav".format(i), 11025, X[:, i])
+    scipy.io.wavfile.write("./Signals/input/mixed_signal_{}.wav".format(i), 11025, X[:, i])
 
-# perform independet component analysis
-ica = ICA(alpha=0.1, method="logistic", iterations=50)
+# perform independet component analysis on X
+ica = ICA(alpha=0.1, method="logistic", iterations=10)
 ica.fit(X)
 S = ica.transform(X)
 
 # write recovered source back to wave file
 for i in range(S.shape[0]):
-    scipy.io.wavfile.write("./Signals/output/unmixed_{}.wav".format(i), 11025, S[i, :])
+    scipy.io.wavfile.write("./Signals/output/unmixed_signal_{}.wav".format(i), 11025, S[i, :])
