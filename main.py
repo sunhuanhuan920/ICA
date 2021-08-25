d2a4fc08eb98f6f231ed310e474fa5bb53aa0adb
Zhenhuann Sun <sunsteven11@gmail.com>
2021-08-24 21:00:23 -0400

    Updated main.py and created Image folder

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 from ICA import ICA
 import numpy as np
 import scipy.io.wavfile # for reading and writing wave file
+import matplotlib.pyplot as plt
 
 # load data
 X = np.loadtxt("./Data/mix.dat")
@@ -25,3 +26,28 @@ S = ica.transform(X)
 # write recovered source back to wave file
 for i in range(S.shape[0]):
     scipy.io.wavfile.write("./Signals/output/unmixed_signal_{}.wav".format(i), 11025, S[i, :])
+
+# number of samples in audio signal
+n_samples = X.shape[0]
+# sampling rate of audio signal (11025 samples/s)
+sample_rate = 11025
+# duration of audio signal in second
+duration = n_samples / sample_rate
+# extend the duration to evenly spaced values to represent time
+time = np.arange(0, duration, 1/sample_rate)
+
+# plot audio signal
+fig, axes = plt.subplots(5, 2)
+for r in range(5):
+    for c in range(2):
+        if c == 0:
+            axes[r, c].plot(time, X[:, r])
+            axes[r, c].set_title("Mixed Signal {}".format(r))
+        else:
+            axes[r, c].plot(time, S[r, :])
+            axes[r, c].set_title("Unmixed Signal {}".format(r))
+
+for ax in axes.flat:
+    ax.set(xlabel="Time (s)", ylabel="Amplitude")
+
+plt.show()
